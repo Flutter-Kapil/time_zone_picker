e6badeae7364a12a6361293c38adaf6d6f7e0199
kapil jhajhria <jhajhria44@gmail.com>
2019-11-06 16:24:10 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Select time zone'),
+          actions: <Widget>[
+            Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            Icon(Icons.more_vert, color: Colors.white)
+          ],
+        ),
+      ),
+    );
+  }
+}
